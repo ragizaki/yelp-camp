b2653382a2c35281744c08f005ed0669c9a91cf2
Zaki Machfj <zakimachfj@gmail.com>
2021-03-25 23:56:45 -0400

    Host Mongo Atlas database

--- app.js
+++ app.js
@@ -15,12 +15,14 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const expressMongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const dbURL = process.env.DB_URL;
+// 'mongodb://localhost:27017/yelp-camp'
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
